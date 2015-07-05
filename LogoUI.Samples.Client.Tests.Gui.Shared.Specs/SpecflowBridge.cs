c54085f55e433bc1f505987ea899b0d661478798
godrose <godrose@gmail.com>
2015-07-05 15:01:46 +0300

    split specs and steps

---
+++ LogoUI.Samples.Client.Tests.Gui.Shared.Specs/SpecflowBridge.cs
@@ -0,0 +1,10 @@
+﻿using TechTalk.SpecFlow;
+
+namespace LogoUI.Samples.Client.Tests.Gui.Shared.Specs
+{    
+    [Binding]
+    class SpecflowBridge : IntegrationTestsBase
+    {
+        
+    }
+}
---
+++ LogoUI.Samples.Client.Tests.Gui.Shared.Steps/GeneralSteps.cs
@@ -0,0 +1,18 @@
+﻿using Attest.Tests.Specflow;
+using Caliburn.Micro;
+using TechTalk.SpecFlow;
+
+namespace LogoUI.Samples.Client.Tests.Gui.Shared.Steps
+{
+    [Binding]
+    class GeneralSteps
+    {
+        [When(@"I open the application")]
+        public void WhenIOpenTheApplication()
+        {
+            ScenarioHelper.CreateRootObject();
+            StructureHelper.SetRootObject(ScenarioHelper.RootObject);
+            ScreenExtensions.TryActivate(ScenarioHelper.RootObject);
+        }
+    }
+}
